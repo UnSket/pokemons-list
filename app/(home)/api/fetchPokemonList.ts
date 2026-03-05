e6e8dec46b1573b1816783124af3e0ab5138c00a
unsket <unsket@mail.ru>
2026-03-05 12:11:04 +0100

    add cache policy

--- app/(home)/api/fetchPokemonList.ts
+++ app/(home)/api/fetchPokemonList.ts
@@ -21,13 +21,13 @@ export interface PokemonListApi {
 }
 
 export async function fetchPokemonList(): Promise<PokemonListApi> {
-	const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
+	const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100", { cache: 'force-cache' });
 	const data = await response.json();
 	const results = await Promise.all(
 		data.results.map((item: { name: string; url: string }) =>
-			fetch(item.url).then((res) => res.json())
+			fetch(item.url, { cache: 'force-cache' }).then((res) => res.json())
 		)
 	);
-    
+
 	return { ...data, results };
 }
